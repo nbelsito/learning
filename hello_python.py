6657a018615eed0e0875f6485b8459eb8e0d31a1
Noah Belsito <nbelsito02@gmail.com>
2022-03-14 04:08:59 +0800

    hello python

---
+++ hello_python.py
@@ -0,0 +1,29 @@
+# variable assignment
+spam_amount = 0
+# print function
+print(spam_amount)
+
+# reassignment and arithmetic operator
+spam_amount = spam_amount + 4
+
+# conditional statement
+if spam_amount > 0:
+    print("But I don't want ANY spam!")
+
+# string arithmetic operation
+viking_song = "Spam " * spam_amount
+print(viking_song)
+
+"""
+Operator  Name 	        Description
+a + b 	Addition 	    Sum of a and b
+a - b 	Subtraction 	Difference of a and b
+a * b 	Multiplication 	Product of a and b
+a / b 	True division 	Quotient of a and b
+a // b 	Floor division 	Quotient of a and b, removing fractional parts
+a % b 	Modulus 	    Integer remainder after division of a by b
+a ** b 	Exponentiation 	a raised to the power of b
+-a 	    Negation 	    The negative of a
+"""
+
+# PEMDAS is order of operation
