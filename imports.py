008a0affeeb6a31b45d8b99918e3621747d0614d
Noah Belsito <nbelsito02@gmail.com>
2022-03-24 04:32:40 +0800

    imports

---
+++ imports.py
@@ -0,0 +1,11 @@
+import math as mt
+from math import *
+
+math = mt
+print("It's math! It has {}".format(type(mt)))
+print(dir(mt))
+print("pi to 4 significant digits = {:.4}".format(mt.pi))
+mt.log(32, 2)
+help(mt.log)
+help(mt)
+print(pi, log(32, 2))
