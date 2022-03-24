4a4025dc95abd8087b6bca3fe3c2e78f25bf2c72
Noah Belsito <nbelsito02@gmail.com>
2022-03-24 09:07:17 +0800

    split data

--- intro_ml/first_ml_model.py
+++ intro_ml/first_ml_model.py
@@ -1,5 +1,7 @@
 import pandas as pd
 from sklearn.tree import DecisionTreeRegressor
+from sklearn.metrics import mean_absolute_error
+from sklearn.model_selection import train_test_split
 
 melbourne_file_path = 'melb_data.csv'
 melbourne_data = pd.read_csv(melbourne_file_path)
@@ -27,3 +29,13 @@ print("Making predictions for the following 5 houses:")
 print(X.head())
 print("The predictions are")
 print(melbourne_model.predict(X.head()))
+
+predicted_home_prices = melbourne_model.predict(X)
+print(mean_absolute_error(y, predicted_home_prices))
+
+train_X, val_X, train_y, val_y = train_test_split(X, y, random_state=0)
+melbourne_model = DecisionTreeRegressor()
+melbourne_model.fit(train_X, train_y)
+val_predictions = melbourne_model.predict(val_X)
+print(mean_absolute_error(val_y, val_predictions))
+
